4dab116b9e0700798fee6795b39150035f69a644
AnOrdinaryUsername <kylemasa2@gmail.com>
2020-09-19 14:31:37 -0700

    Add new properties and methods to UserInput class

--- src/controller/UserInput.ts
+++ src/controller/UserInput.ts
@@ -1,12 +1,26 @@
 import Answer from "./Answer";
 import Problem from "./Problem";
+import Score from "./Score";
 
 export default class UserInput {
-  public onSubmit = (question: Problem): void => {
+  private _currentProblem: Problem;
+  private _userScore: Score;
+  private _submitState: boolean; // Boolean to check if user has submitted already.
+
+  constructor(problem: Problem) {
+    this._currentProblem = problem;
+    this._userScore = new Score();
+    this._submitState = false;
+  }
+
+  public onSubmit = (): void => {
     document.getElementById("user-answer").addEventListener("submit", (e) => {
       // once submitted, check if answer is correct + display box with feedback
       e.preventDefault();
-      const a: Answer = new Answer(this, question);
+
+      if (this._submitState === true) return;
+
+      const a: Answer = new Answer(this, this._currentProblem);
       const tip = document.querySelector(".tip");
 
       if (this.grabInput() === null) {
@@ -14,13 +28,39 @@ export default class UserInput {
         return;
       }
 
-      if (a.isCorrect()) tip.textContent = "Woah epic move!";
-      else tip.textContent = "Sadge not right";
+      if (a.isCorrect()) {
+        tip.textContent = "Woah epic move!";
+        this._userScore.incrementRightCount();
+      } else {
+        tip.textContent = "Sadge not right";
+        this._userScore.incrementWrongCount();
+        this._currentProblem.pushQuestionGotWrong(
+          this._currentProblem.question
+        );
+      }
 
       document.querySelector(".test").textContent = a.solution;
       document.querySelector(
         ".test2"
       ).textContent = a.solution.length.toString();
+
+      document
+        .querySelector(".result button")
+        .classList.toggle("hide-visibility");
+
+      this._submitState = true;
+    });
+  };
+
+  public showNextQuestion = (): void => {
+    document.querySelector(".result button").addEventListener("click", () => {
+      this._submitState = false;
+
+      this._currentProblem.erasePreviousQuestion();
+      this._currentProblem.generateProblem();
+      document
+        .querySelector(".result button")
+        .classList.toggle("hide-visibility");
     });
   };
 
