0e1e8879444b80963787b1063308fe291735a5b5
AnOrdinaryUsername <kylemasa2@gmail.com>
2020-09-19 14:29:46 -0700

    Include property to store questions user got wrong

--- src/controller/Problem.ts
+++ src/controller/Problem.ts
@@ -1,10 +1,12 @@
 import UserChoice from "./UserChoice";
 
 export default class Problem {
+  private _wrongAnswers: string[];
   private _question: string;
   public selection: UserChoice;
 
   constructor(select: UserChoice) {
+    this._wrongAnswers = [];
     this._question = "";
     this.selection = select;
   }
@@ -15,11 +17,27 @@ export default class Problem {
     return randNum.toString(this.selection.oldBase).toUpperCase();
   };
 
+  public erasePreviousQuestion = (): void => {
+    this._question = "";
+  };
+
   public generateProblem = (amount = 4): void => {
     for (let i = 0; i < amount; ++i) {
       this._question += this.getRandomDigit();
     }
+
+    const oldBase = this.selection.oldBase.toString();
+    const newBase = this.selection.newBase.toString();
+
+    document.getElementById("question").textContent = "";
     document.getElementById("question").textContent = this.question;
+    document.getElementById("base-1").textContent = oldBase;
+    document.getElementById("base-2").textContent = newBase;
+  };
+
+  public pushQuestionGotWrong = (question: string): void => {
+    this._wrongAnswers.push(question);
+    console.log(this._wrongAnswers);
   };
 
   public get question(): string {
