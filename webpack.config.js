0f7698e0c81fc2fbefc9781b341827d0000376a8
AnOrdinaryUsername <kylemasa2@gmail.com>
2020-12-12 16:08:07 -0800

    feat: Update the webpack config
    Include devServer options for Hot Module Replacement. Make sure
    not to remove 'writeToDisk' since, without it, CreateWebpackPlugin
    will delete almost everything in the dist folder.

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,49 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: './src/index.tsx',
   devtool: 'inline-source-map',
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
+        test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
       },
+      // css-loader to bundle all the css files into one file
+      // style-loader to add all the styles  inside the style tag of the document
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
     ],
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    contentBase: './src',
+    inline: true,
+    liveReload: false,
+    writeToDisk: true,
+    open: {
+      app: ['chrome', '--incognito'],
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['**/*', '!images/**'],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+  ]
 };
