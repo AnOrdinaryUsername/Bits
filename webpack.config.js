c33098d04a48de20b36d1946a51617cf5e3954b6
AnOrdinaryUsername <kylemasa2@gmail.com>
2020-12-14 20:24:25 -0800

    feat: Add jsx extension to resolve

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
   },
   output: {
     filename: 'bundle.js',
