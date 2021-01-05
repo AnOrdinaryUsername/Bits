022ef5a02ccc331ad22466acb660d0b39478e076
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-01-04 19:34:21 -0800

    feat: Add react plugin for better linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,12 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: [
       '@typescript-eslint',
+      'react',
     ],
     extends: [
       'eslint:recommended',
       'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
     ],
     env: {
         browser: true,
