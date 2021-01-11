8da3f8ff6ffe8ebb819a1da3226491e49022bbb0
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-01-10 19:33:38 -0800

    feat: Add production options to config
    Change module.exports to a function for env variable to check if
    the build is for production. Use it to check for filename's
    hash mode and to disable source-maps.
    
    When building for prod, inline-source-maps take up a lot of space.
    Disable it for production only.
    
    Use hash-mode for caching. Look here for an explanation
    (https://stackoverflow.com/a/59197260).

--- webpack.config.js
+++ webpack.config.js
@@ -3,58 +3,64 @@ const os = require('os');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.tsx',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      // style-loader to add all the styles  inside the style tag of the document
-      // typings-for-css-modules-loader to generate automatic type definitions
-      // css-loader to bundle all the css files into one file
-      {
-        test: /\.css$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: "@teamsupercell/typings-for-css-modules-loader" },
-          { loader: "css-loader", options: { modules: true } },
-        ]
-      },
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+module.exports = env => {
+  const PRODUCTION = (env === 'production');
+  const HASH_MODE = PRODUCTION ? 'contenthash' : 'chunkhash';
+  
+  return {
+    entry: './src/index.tsx',
+    devtool: (PRODUCTION) ? 'none' : 'inline-source-map',
+    module: {
+      rules: [
+        // style-loader to add all the styles  inside the style tag of the document
+        // typings-for-css-modules-loader to generate automatic type definitions
+        // css-loader to bundle all the css files into one file
+        {
+          test: /\.css$/,
+          use: [
+            { loader: 'style-loader' },
+            { loader: "@teamsupercell/typings-for-css-modules-loader" },
+            { loader: "css-loader", options: { modules: true } },
+          ]
         },
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-  },
-  devServer: {
-    contentBase: './src',
-    inline: true,
-    liveReload: false,
-    writeToDisk: true,
-    historyApiFallback: true,
-    open: {
-      app: [getApp(), '--incognito'],
-    }
-  },
-  plugins: [
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: ['**/*', '!images/**'],
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html"),
-      publicPath: "",
-    }),
-  ]
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    },
+    output: {
+      filename: `[name].[${HASH_MODE}].js`,
+      chunkFilename: `[name].[${HASH_MODE}].js`,
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+    },
+    devServer: {
+      contentBase: './src',
+      inline: true,
+      liveReload: false,
+      writeToDisk: true,
+      historyApiFallback: true,
+      open: {
+        app: [getApp(), '--incognito'],
+      }
+    },
+    plugins: [
+      new CleanWebpackPlugin({
+        cleanOnceBeforeBuildPatterns: ['**/*', '!images/**'],
+      }),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html"),
+        publicPath: "",
+      }),
+    ]
+  }
 };
 
 function getApp() {
