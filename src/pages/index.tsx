70b1ba297ff2906f16caa3ea8b6f65f44e5a72cc
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-01-10 19:54:43 -0800

    refactor: Use lazy loading for better performance
    Use Babel's dynamic import syntax to load the component asynchronously
    and show a Loading icon screen while it waits.
    
    This makes the user experience more pleasurable by reducing the initial
    load time.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,65 @@
-import { Home } from './Home';
-import { HowItWorks } from './HowItWorks';
-import { NotFound } from './NotFound';
-import { Practice } from './Practice';
-import { Resources } from './Resources';
+import loadable from '@loadable/component';
+import React, { ReactElement } from 'react';
+import { Loading } from '../components/Loading';
+
+
+const LoadableHome = loadable(() => import(/* webpackChunkName: "home" */ './Home'), {
+   resolveComponent: (components) => components.Home,
+   fallback: <Loading />, 
+});
+
+const Home = (): ReactElement => {
+    return <LoadableHome />
+};
+
+
+const LoadableHowItWorks = loadable(() => import(/* webpackChunkName: "how-it-works" */ 
+    './HowItWorks'), {
+    resolveComponent: (components) => components.HowItWorks,
+    fallback: <Loading />,
+});
+
+const HowItWorks = (): ReactElement => {
+    return <LoadableHowItWorks />
+};
+
+
+const LoadableNotFound = loadable(() => import(/* webpackChunkName: "not-found" */
+    './NotFound'), {
+    resolveComponent: (components) => components.NotFound,
+    fallback: <Loading />,
+});
+
+const NotFound = (): ReactElement => {
+    return <LoadableNotFound />
+};
+
+
+const LoadablePractice = loadable(() => import(/* webpackChunkName: "practice" */ './Practice'), {
+    resolveComponent: (components) => components.Practice,
+    fallback: <Loading />,
+});
+
+const Practice = (): ReactElement => {
+    return <LoadablePractice />
+};
+
+
+const LoadableResources = loadable(() => import(/* webpackChunkName: "resources" */ 
+    './Resources'), {
+    resolveComponent: (components) => components.Resources,
+    fallback: <Loading />,
+});
+
+const Resources = (): ReactElement => {
+    return <LoadableResources />
+};
 
 export {
     Home,
     HowItWorks,
     Practice,
     Resources,
-    NotFound
+    NotFound,
 };
+
