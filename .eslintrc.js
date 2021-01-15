74783794f06481e1823bf30284153739768bc98b
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-01-14 21:24:16 -0800

    feat: Add react settings to fix warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,10 +9,16 @@ module.exports = {
       'eslint:recommended',
       'plugin:@typescript-eslint/recommended',
       'plugin:react/recommended',
+      'plugin:compat/recommended'
     ],
     env: {
         browser: true,
         amd: true,
         node: true,
     },
+    settings: {
+      react: {
+        version: 'detect',
+      }
+    }
   };
